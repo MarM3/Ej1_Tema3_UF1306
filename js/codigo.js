257131dd8f15f59062ea53e836111958f6bb9969
Sheila Adr <siladr2019@gmail.com>
2019-05-22 13:36:20 +0200

    Cambios en css (class) y en la función Edad

--- js/codigo.js
+++ js/codigo.js
@@ -3,7 +3,7 @@
 // Función para validar que la edad introducida no este en blanco y sean números
 var txtEdad = document.getElementById("campoEdad")
 function validarEdad() {
-    if (txtEdad == null || txtEdad.length == 0 || isNaN(txtEdad)) {
+    if (txtEdad === null || txtEdad.length === 0 || isNaN(txtEdad)) {
         alert('ERROR: Debes introducir una edad');
         return false;
     }
