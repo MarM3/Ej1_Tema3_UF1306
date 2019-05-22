4605a5081b5ceff520467aaa5a97ade81ecced66
Equipo Cecilia, Sheila y Mar <mm.zlsmng@gmail.com>
2019-05-22 13:12:43 +0200

    Funciones ciudad y telefono

--- js/codigo.js
+++ js/codigo.js
@@ -1,1 +1,31 @@
 //Crear funciones para validar los datos introducidos en un formulario
+
+
+function validacionDatos(){
+
+
+    validarCiudad();
+    validarTelefono();
+}
+
+function validarCiudad(){
+    let ciudad = document.getElementById("campoCiudad");
+    const PATRON = /^\s+[0-9]\s+$/;
+
+    if (ciudad.value!=="" || !PATRON.test(ciudad.value)){
+        document.getElementById("campoCiudad").className = "bien";
+    } else {
+        document.getElementById("campoCiudad").className = "mal";
+    }
+}
+
+function validarTelefono(){
+    let telefono = document.getElementById("campoTelefono").value;
+    const PATRON = /^5{3}\s\d{6}$/;
+
+    if (PATRON.test(telefono)){
+        document.getElementById("campoTelefono").className = "bien";
+    } else {
+        document.getElementById("campoTelefono").className = "mal";
+    }
+}
