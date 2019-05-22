92efe84903015d3cb218974d66137537aa00283d
Sheiila Adr <siladr2019@gmail.com>
2019-05-22 13:03:37 +0200

    Función para comprobar el campoEdad

--- js/codigo.js
+++ js/codigo.js
@@ -1,1 +1,10 @@
 //Crear funciones para validar los datos introducidos en un formulario
+
+// Función para validar que la edad introducida no este en blanco y sean números
+var txtEdad = document.getElementById("campoEdad")
+function validarEdad() {
+    if (txtEdad == null || txtEdad.length == 0 || isNaN(txtEdad)) {
+        alert('ERROR: Debes introducir una edad');
+        return false;
+    }
+}
