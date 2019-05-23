2b259dd7e2b9fc90c690857599b4b0af4b7b1d00
Equipo Cecilia, Sheila y Mar <mm.zlsmng@gmail.com>
2019-05-23 10:17:58 +0200

    Ultimo commit correcciones

--- js/codigo.js
+++ js/codigo.js
@@ -27,43 +27,41 @@
 
 
 
+let txtEdad = document.getElementById("campoEdad");
+const patron = /^\d[1-9]{1, 3}$/;
+txtEdad.onblur = validarEdad;
 
-
-
-
-
-
-
-
-
+function validarEdad() {
+   if (txtEdad.value === "" || patron.test(txtEdad.value)) {
+       document.getElementById("campoEdad").className = "mal";
+   } else {
+       document.getElementById("campoEdad").className = "bien";
+   }
+}
 
 //Crear funciones para validar los datos introducidos en un formulario
-let ciudad = document.getElementById("campoCiudad");
-const PATRON = /^\s+[0-9]\s+$/;
-const validar = document.getElementById("campoCiudad");
-validar.onblur = validarCiudad;
+const ciudad = document.getElementById("campoCiudad");
+ciudad.onblur = validarCiudad;
 
-/*validaciones("campoCiudad", validarCiudad);
+function validarCiudad(){
+    ciudad.value = ciudad.value.trim(); //le quitamos los espacios por delante y por detrás
+    const PATRON = /^\s+[0-9]+\s+$/;
+    const PATRON2 = /^[A-Za-zÑñÁáÉéÍíÓóÚúüÜ]+$/;
 
-function validaciones(elemento, fx){
-    const validar = document.getElementById(elemento);
-    validar.onblur = fx;
-}*/
-
-function validarCiudad(evento){
-
-    if (ciudad.value !== "" || !PATRON.test(ciudad.value)) {
+    if (ciudad.value && PATRON2.test(ciudad.value))) {// || !PATRON.test(ciudad.value)  || !PATRON2.test(ciudad.value
         document.getElementById("campoCiudad").className = "bien";
     } else {
         document.getElementById("campoCiudad").className = "mal";
     }
 }
 
+const telefono = document.getElementById("campoTelefono");
+telefono.onblur = validarTelefono;
+
 function validarTelefono() {
-    let telefono = document.getElementById("campoTelefono").value;
     const PATRON = /^5{3}\s\d{6}$/;
 
-    if (PATRON.test(telefono)) {
+    if (PATRON.test(telefono.value)) {
         document.getElementById("campoTelefono").className = "bien";
     } else {
         document.getElementById("campoTelefono").className = "mal";
